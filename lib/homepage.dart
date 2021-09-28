25f44bd9cf2fbc7971b08a1e69a974848ba3e502
Bakti <baktipraz0@gmail.com>
2021-09-28 15:47:31 +0700

    Progress2

---
+++ lib/homepage.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class HalHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
---
+++ lib/welcomepage.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+// class WelcomePage extends StatelessWidget {
+//   const WelcomePage({ Key? key }) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Stack(
+//         fit: StackFit.expand,
+//         children: <Widget>[
+//           Container(
+//             width: MediaQuery.of(context).size.width,
+//             height: MediaQuery.of(context).size.height,
+//             color: Colors.black12,
+//           ),
+//         ],
+//       ),
+      
+//     );
+//   }
+// }
