cec1d0b7549c5e2aab71ffbeca7156bc31c7b590
Bakti <baktipraz0@gmail.com>
2021-09-28 15:47:14 +0700

    Progress2

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 // ignore_for_file: unused_element
 
 import 'dart:async';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:antriyuk/welcomepage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,88 +20,58 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: WelcomePage(),
     );
   }
 }
 
-class LoginPage extends StatefulWidget {
-  @override
-  _LoginPageState createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-  final GoogleSignIn googleSignIn = new GoogleSignIn();
-
-  Future<User?> _signIn() async {
-    final GoogleSignInAccount? account = await googleSignIn.signIn();
-    final GoogleSignInAuthentication authentication =
-        await account!.authentication;
-
-    final OAuthCredential credential = GoogleAuthProvider.credential(
-        idToken: authentication.idToken,
-        accessToken: authentication.accessToken);
-
-    // final UserCredential authResult =
-    //     await _auth.signInWithCredential(credential);
-    // final User? user = authResult.user;
-  }
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(
-      fit: StackFit.expand,
-      alignment: Alignment.center,
-      children: [
-        Container(
-          alignment: Alignment.center,
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          // color: Color(0xffdc1b1b),
-        ),
-        Column(children: <Widget>[
-          Spacer(flex: 20),
-          Text('Login With Google'),
-          Spacer(
-            flex: 1,
+      body: Stack(
+        fit: StackFit.expand,
+        alignment: Alignment.center,
+        children: <Widget>[
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            color: Color(0xff),
           ),
-          SizedBox(
-            width: MediaQuery.of(context).size.width * 0.6,
-            child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.white60,
-                  padding: EdgeInsets.all(10),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Spacer(
+                flex: 10,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.5,
+                height: MediaQuery.of(context).size.height * 0.2,
+                child: Image(
+                  image: AssetImage('assets/queue.png'),
+                  fit: BoxFit.contain,
                 ),
-                onPressed: () => _signIn(),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Spacer(),
-                    Icon(
-                      FontAwesomeIcons.google,
-                      color: Colors.red,
-                    ),
-                    Spacer(),
-                    Text(
-                      'Login with Google',
-                      style: TextStyle(
-                        color: Colors.black,
-                      ),
-                    ),
-                    Spacer()
-                  ],
-                )),
+              ),
+              Spacer(
+                flex: 1,
+              ),
+              Text(
+                'Antri Yuk',
+                style: TextStyle(
+                    fontSize: 40,
+                    color: Color(0xffdd0303),
+                    fontWeight: FontWeight.bold,
+                    fontFamily: "OpenSansCondensed-Light"),
+              ),
+              Spacer(
+                flex: 10,
+              )
+            ],
           ),
-          Spacer(
-            flex: 20,
-          )
-        ]),
-      ],
-    ));
+        ],
+      ),
+    );
   }
 }
