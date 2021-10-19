8d69d09b7695bfd369cd3552933041a10252f02c
Bakti <baktipraz0@gmail.com>
2021-10-19 13:10:00 +0700

    perubahan 4

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,9 +1,84 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class HalHome extends StatelessWidget {
+  final user = FirebaseAuth.instance.currentUser!;
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return Scaffold(
+        body: Stack(
+      alignment: Alignment.topCenter,
+      fit: StackFit.expand,
+      children: <Widget>[
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height * 0.20,
+              decoration: BoxDecoration(
+                color: Color(0xffDC1B1B),
+                borderRadius: BorderRadius.vertical(
+                  bottom:
+                      Radius.elliptical(MediaQuery.of(context).size.width, 100),
+                ),
+              ),
+            ),
+            Spacer(),
+          ],
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              height: MediaQuery.of(context).size.height * 0.115,
+              width: MediaQuery.of(context).size.width,
+              alignment: Alignment.bottomCenter,
+              child: SizedBox(
+                height: MediaQuery.of(context).size.height * 0.065,
+                width: MediaQuery.of(context).size.width,
+                child: Row(
+                  children: <Widget>[
+                    Spacer(
+                      flex: 1,
+                    ),
+                    CircleAvatar(
+                      radius: 40,
+                      backgroundColor: Colors.grey,
+                      backgroundImage: NetworkImage(user.photoURL!),
+                    ),
+                    Spacer(
+                      flex: 1,
+                    ),
+                    Text(
+                      user.displayName!,
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Spacer(
+                      flex: 10000,
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              height: MediaQuery.of(context).size.height * 0.2,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(border: Border.all(color: Colors.red)),
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: <Widget>[],
+              ),
+            )
+          ],
+        )
+      ],
+    ));
   }
 }
